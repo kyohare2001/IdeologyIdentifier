7e12398f67415df22e52fb0804add913f2d6bd82
kyohare2001 <kyohareda@icloud.com>
2025-04-16 17:55:08 -0500

    Rename Dockerfile

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,11 +1,19 @@
 
-document.getElementById('submitBtn').addEventListener('click', function(event) {
+document.getElementById('submitBtn').addEventListener('click', async function(event) {
     var inputValue = document.getElementById('userInput').value;
     var data = {data: inputValue};
 
-    console.log(data.data);cd 
-})
-
-app.get('/api/data', (req, res) => {
-    res.send(data.data);
+    try {
+        // As a JSON file send data to the server
+        const res = await fetch('/api/data', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        });
+        // async function は非同期処理を行うための関数です。await キーワードを使用して、Promiseが解決されるのを待ちます。
+        const json = await res.json();
+        console.log('サーバー応答:', json);
+      } catch (err) {
+        console.error('送信エラー:', err);
+      }
 })
